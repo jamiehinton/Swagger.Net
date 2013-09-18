909ebd17e1888abb6802999ea339a1eaf4fca60a
jhinton <jhinton@technophobia.com>
2013-09-18 15:00:49 +0100

    added the ability to override the base path - without this there are issues with azure emulator as varied ports are given (81 and 82 in my case)

--- Swagger.Net/SwaggerModels.cs
+++ Swagger.Net/SwaggerModels.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.Http.Controllers;
-using System.Web.Http.Description;
-
-namespace Swagger.Net
+﻿namespace Swagger.Net
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Description;
+
     public static class SwaggerGen
     {
         public const string SWAGGER = "swagger";
@@ -24,9 +24,10 @@ namespace Swagger.Net
         /// <param name="actionContext">Current action context</param>
         /// <param name="includeResourcePath">Should the resource path property be included in the response</param>
         /// <returns>A resource Listing</returns>
-        public static ResourceListing CreateResourceListing(HttpActionContext actionContext, bool includeResourcePath = true)
+        public static ResourceListing CreateResourceListing( HttpActionContext actionContext,
+                                                             bool includeResourcePath = true )
         {
-            return CreateResourceListing(actionContext.ControllerContext, includeResourcePath);
+            return CreateResourceListing( actionContext.ControllerContext, includeResourcePath );
         }
 
         /// <summary>
@@ -35,21 +36,32 @@ namespace Swagger.Net
         /// <param name="actionContext">Current controller context</param>
         /// <param name="includeResourcePath">Should the resource path property be included in the response</param>
         /// <returns>A resrouce listing</returns>
-        public static ResourceListing CreateResourceListing(HttpControllerContext controllerContext, bool includeResourcePath = false)
+        public static ResourceListing CreateResourceListing( HttpControllerContext controllerContext,
+                                                             bool includeResourcePath = false )
         {
-            Uri uri = controllerContext.Request.RequestUri;
+            ResourceListing rl = new ResourceListing
+                {
+                    apiVersion = Assembly.GetCallingAssembly().GetType().Assembly.GetName().Version.ToString(),
+                    swaggerVersion = SWAGGER_VERSION,
+                    basePath = GenerateBasePathOrUseOverride( controllerContext ),
+                    apis = new List< ResourceApi >()
+                };
 
-            ResourceListing rl = new ResourceListing()
-            {
-                apiVersion = Assembly.GetCallingAssembly().GetType().Assembly.GetName().Version.ToString(),
-                swaggerVersion = SWAGGER_VERSION,
-                basePath = uri.GetLeftPart(UriPartial.Authority) + HttpRuntime.AppDomainAppVirtualPath.TrimEnd('/'),
-                apis = new List<ResourceApi>()
-            };
-
-            if (includeResourcePath) rl.resourcePath = controllerContext.ControllerDescriptor.ControllerName;
+            if( includeResourcePath ) rl.resourcePath = controllerContext.ControllerDescriptor.ControllerName;
 
             return rl;
+        }
+
+        private static string GenerateBasePathOrUseOverride( HttpControllerContext controllerContext )
+        {
+            if( HttpRuntime.AppDomainAppVirtualPath == null )
+                throw new NullReferenceException( "HttpRuntime is null, Swagger.Net will only work in an Http context." );
+
+            var uri = controllerContext.Request.RequestUri;
+            var forcedSwaggerBaseUri = ConfigurationManager.AppSettings[ "SwaggerBaseUri" ];
+            return string.IsNullOrWhiteSpace( forcedSwaggerBaseUri )
+                       ? string.Format( "{0}{1}", uri.GetLeftPart( UriPartial.Authority ), HttpRuntime.AppDomainAppVirtualPath.TrimEnd( '/' ) )
+                       : forcedSwaggerBaseUri;
         }
 
         /// <summary>
@@ -57,14 +69,14 @@ namespace Swagger.Net
         /// </summary>
         /// <param name="api">Description of the api via the ApiExplorer</param>
         /// <returns>A resource api</returns>
-        public static ResourceApi CreateResourceApi(ApiDescription api)
+        public static ResourceApi CreateResourceApi( ApiDescription api )
         {
-            ResourceApi rApi = new ResourceApi()
-            {
-                path = "/" + api.RelativePath,
-                description = api.Documentation,
-                operations = new List<ResourceApiOperation>()
-            };
+            ResourceApi rApi = new ResourceApi
+                {
+                    path = "/" + api.RelativePath,
+                    description = api.Documentation,
+                    operations = new List< ResourceApiOperation >()
+                };
 
             return rApi;
         }
@@ -75,17 +87,18 @@ namespace Swagger.Net
         /// <param name="api">Description of the api via the ApiExplorer</param>
         /// <param name="docProvider">Access to the XML docs written in code</param>
         /// <returns>An api operation</returns>
-        public static ResourceApiOperation CreateResourceApiOperation(ApiDescription api, XmlCommentDocumentationProvider docProvider)
+        public static ResourceApiOperation CreateResourceApiOperation( ApiDescription api,
+                                                                       XmlCommentDocumentationProvider docProvider )
         {
-            ResourceApiOperation rApiOperation = new ResourceApiOperation()
-            {
-                httpMethod = api.HttpMethod.ToString(),
-                nickname = docProvider.GetNickname(api.ActionDescriptor),
-                responseClass = docProvider.GetResponseClass(api.ActionDescriptor),
-                summary = api.Documentation,
-                notes = docProvider.GetNotes(api.ActionDescriptor),
-                parameters = new List<ResourceApiOperationParameter>()
-            };
+            ResourceApiOperation rApiOperation = new ResourceApiOperation
+                {
+                    httpMethod = api.HttpMethod.ToString(),
+                    nickname = docProvider.GetNickname( api.ActionDescriptor ),
+                    responseClass = docProvider.GetResponseClass( api.ActionDescriptor ),
+                    summary = api.Documentation,
+                    notes = docProvider.GetNotes( api.ActionDescriptor ),
+                    parameters = new List< ResourceApiOperationParameter >()
+                };
 
             return rApiOperation;
         }
@@ -97,17 +110,23 @@ namespace Swagger.Net
         /// <param name="param">Description of a parameter on an operation via the ApiExplorer</param>
         /// <param name="docProvider">Access to the XML docs written in code</param>
         /// <returns>An operation parameter</returns>
-        public static ResourceApiOperationParameter CreateResourceApiOperationParameter(ApiDescription api, ApiParameterDescription param, XmlCommentDocumentationProvider docProvider)
+        public static ResourceApiOperationParameter CreateResourceApiOperationParameter( ApiDescription api,
+                                                                                         ApiParameterDescription param,
+                                                                                         XmlCommentDocumentationProvider
+                                                                                             docProvider )
         {
-            string paramType = (param.Source.ToString().Equals(FROMURI)) ? QUERY : BODY;
-            ResourceApiOperationParameter parameter = new ResourceApiOperationParameter()
-            {
-                paramType = (paramType == "query" && api.RelativePath.IndexOf("{" + param.Name + "}") > -1) ? PATH : paramType,
-                name = param.Name,
-                description = param.Documentation,
-                dataType = param.ParameterDescriptor.ParameterType.Name,
-                required = docProvider.GetRequired(param.ParameterDescriptor)
-            };
+            string paramType = ( param.Source.ToString().Equals( FROMURI ) ) ? QUERY : BODY;
+            ResourceApiOperationParameter parameter = new ResourceApiOperationParameter
+                {
+                    paramType =
+                        ( paramType == "query" && api.RelativePath.IndexOf( "{" + param.Name + "}" ) > -1 )
+                            ? PATH
+                            : paramType,
+                    name = param.Name,
+                    description = param.Documentation,
+                    dataType = param.ParameterDescriptor.ParameterType.Name,
+                    required = docProvider.GetRequired( param.ParameterDescriptor )
+                };
 
             return parameter;
         }
@@ -119,14 +138,14 @@ namespace Swagger.Net
         public string swaggerVersion { get; set; }
         public string basePath { get; set; }
         public string resourcePath { get; set; }
-        public List<ResourceApi> apis { get; set; }
+        public List< ResourceApi > apis { get; set; }
     }
 
     public class ResourceApi
     {
         public string path { get; set; }
         public string description { get; set; }
-        public List<ResourceApiOperation> operations { get; set; }
+        public List< ResourceApiOperation > operations { get; set; }
     }
 
     public class ResourceApiOperation
@@ -136,7 +155,7 @@ namespace Swagger.Net
         public string responseClass { get; set; }
         public string summary { get; set; }
         public string notes { get; set; }
-        public List<ResourceApiOperationParameter> parameters { get; set; }
+        public List< ResourceApiOperationParameter > parameters { get; set; }
     }
 
     public class ResourceApiOperationParameter
